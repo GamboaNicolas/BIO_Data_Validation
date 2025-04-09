5742d86178ac363e0d9287ea39633bc6b08c5892
ro-canteros <rycanteros@gmail.com>
2025-04-09 18:57:56 -0300

    Revisa todo mis comentarios, porfi

--- Pruebas.R
+++ Pruebas.R
@@ -1,4 +1,4 @@
-3library(dplyr)
+library(dplyr)
 library(stringr)
 library(lubridate)
 library(readxl)
@@ -17,10 +17,107 @@ reglas <-  tribble(
   "r7", "Elegible USSCR dentro de opciones",        "!(usscr %in% c(1,2))",
   "r8", "Consentimiento dentro de opciones",        "!(consent %in% c(1,2))",
   "r9", "Número del sujeto en el formato adecuado", "stringr::str_detect(subjectnumber,pattern = '^[:digit:]{9}$', negate = T)",
-  "r10", "Enrolamiento correcto",                   "scr == 2 && usscr == 2 && consent == 2",
-  "r11", "Número del sujeto correcto",              "substr(subjectnumber, 1, 2) == countrycode | substr(subjectnumber, 1, 2) == countrycode",
-  "r12", "Grupo étnico respondido",                 "ethnicgroup != NA",
-  "r13", "Número del sujeto completado",            "subjectnumber != NA"
+  "r10", "Enrolamiento correcto",                   "scr != 2 | usscr != 2 | consent != 2",
+  "r11", "Número del sujeto correcto",              "substr(subjectnumber, 1, 3) != paste0(0,countrycode) & substr(subjectnumber, 1, 3) != paste0(0, 0, countrycode)",
+  "r12", "Grupo étnico respondido",                 "is.na(ethnicgroup)",
+  "r13", "Número del sujeto completado",            "is.na(subjectnumber)",
+  "r14", "Código de país completado",               "is.na(countrycode)"
   )
 
+# ==============================================================================
+#                                   VALIDACIÓN
+# ==============================================================================
 
+# Funcion validador()
+# argumentos:
+# - datos: conjunto de validacion
+# - id : nombre de la columna en (datos) con el identificador
+# - cond : nombre de la columna en (datos) con la condicion
+# salida: vector nombrado
+validador = function(datos, id, cond){
+  reglas = datos[[cond]]
+  names(reglas) = datos[[id]]
+  reglas
+}
+
+# Funcion validar()
+# argumentos:
+# - datos : conjunto de datos a validar
+# - id : nombre de la columna en (datos) con el identificador
+# - validador: salida de validador()
+# salida: tibble con el resultado de la validación
+validar = function(datos, id, validador){
+  sapply(
+    validador,
+    function(x) eval(parse(text = x), datos)
+  ) |>
+    dplyr::as_tibble() |>
+    dplyr::mutate(registro = datos[[id]], .before = 0)
+}
+
+
+validacion <-  validar(datos, "id", validador(reglas, "id", "cond"))
+
+
+
+
+# GRÁFICO por la gran cantidad de datos
+# Para poder hacerlo (si es que lo vamos a presentar) tenemos que dividir entre 
+# las reglas donde "FALSE" es positivo y donde "FALSE" es negativo (es decir, 
+# por un lado las reglas que están bien que nos den FALSE y las que no); de ese 
+# modo no se van a presentar confusiones.
+
+# Reglas FALSE POSITIVO: r1, r4, r5, r6, r7, r8, r10, r11, r12, r13, r14
+# Reglas FALSE NEGATIVO: r2, r3, r9 (están dudosas pq no entiendo cómo funciona lo que pusiste)
+
+# -REGLAS CASO 1-
+# 1ro: se pasa a formato largo
+validacion_largo_1 <-  tidyr::pivot_longer(validacion[1:100,c(1,2, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15)], -registro, names_to = "regla", values_to = "error")
+
+# 2do: se grafica
+library(ggplot2)
+ggplot(validacion_largo_1)+
+  aes(x = registro, y = regla, fill = error)+
+  geom_tile(width = 0.7, height = .9, color = "black")+
+  scale_fill_manual(values = c("#377EB8","#E41A1C","#999999"))+
+  theme(caption = "Gráfico 1: Falso = bueno") +
+  theme_bw()
+
+
+# -REGLAS CASO 2-
+# 1ro: se pasa a formato largo
+validacion_largo_2 <-  tidyr::pivot_longer(validacion[1:100,c(1,3, 4, 10)], -registro, names_to = "regla", values_to = "error")
+
+# 2do: se grafica
+library(ggplot2)
+ggplot(validacion_largo_2)+
+  aes(x = registro, y = regla, fill = error)+
+  geom_tile(width = 0.7, height = .9, color = "black")+
+  scale_fill_manual(values = c("#E41A1C", "#377EB8","#999999"))+
+  theme(caption = "Gráfico 2: Falso = malo") +
+  theme_bw()
+
+
+# RESULTADOS
+
+# 1) Participantes limpios
+# Al igual que el gráfico, hay que contar por un lado aquellos individuos donde
+# el TRUE significa error y lo que no (en este último caso sumamos los TRUE y 
+# sumamos a individuos limpios)
+
+# -CASO 1-
+validacion_largo_1 |>
+  filter(error) |>
+  distinct(registro) |>
+  count()
+# Hay 22 individuos con inconsistencias -> hay, en este caso, 978 limpios
+
+
+# -CASO 2-
+validacion_largo_2 |>
+  filter(error) |>
+  distinct(registro) |>
+  count()
+# Hay 100 individuos con todo "TRUE" -> hay 100 individuos limpios
+
+# ¿Cómo saber cuáles coinciden con los 978 de arriba? NICO, HELP.
