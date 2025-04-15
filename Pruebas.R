b71b27a92cbecfe4d278fb897efb409a09b3f91e
GamboaNicolas <gamboa.nicolas2002@gmail.com>
2025-04-15 19:10:17 +0000

    Correcciones del Script. Gráfico nuevo

--- Pruebas.R
+++ Pruebas.R
@@ -2,33 +2,36 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 library(readxl)
+library(patchwork)
+library(egg)
+
 datos <- read_xlsx("Archivos/adm.xlsx") |> 
   mutate(id = 1:n())
 
 
 reglas <-  tribble(
   ~id,  ~desc,                                      ~cond,          
-  "r1", "pais fuera de la lista",                   "!(countrycode %in% c(4, 11, 14, 23, 31, 48, 54, 65, 72, 97))",
-  "r2", "ID en formato DD/MM/YYYY-AA",              "stringr::str_detect(patientid,pattern = '^[:digit:]{2}/[:digit:]{2}/[:digit:]{4}-[:upper:]{2}$', negate = T) | is.na(dmy(substr(patientid,1,10),quiet = T))",
-  "r3", "Fecha de entrevista en formato DD/MM/YYYY","stringr::str_detect(interview,pattern = '^[:digit:]{2}/[:digit:]{2}/[:digit:]{4}$', negate = T) | is.na(dmy(interview,quiet = T))",
-  "r4", "Fechas congruentes(edad > 18)",            "as.numeric(dmy(interview,quiet = T) - dmy(substr(patientid,1,10),quiet = T))/365 < 18",
-  "r5", "Grupo étnico entre las opciones",          "!(ethnicgroup %in% c(1,2,3,4))",
-  "r6", "Elegible SCR dentro de opciones",          "!(scr %in% c(1,2))",
-  "r7", "Elegible USSCR dentro de opciones",        "!(usscr %in% c(1,2))",
-  "r8", "Consentimiento dentro de opciones",        "!(consent %in% c(1,2))",
-  "r9", "Número del sujeto en el formato adecuado", "stringr::str_detect(subjectnumber,pattern = '^[:digit:]{9}$', negate = T)",
-  # "r10", "Enrolamiento correcto",                   "scr != 2 | usscr != 2 | consent != 2",
-  "r10", "Enrolamiento correcto",                   "((scr == 1 | usscr == 1 | consent == 1) & !is.na(subjectnumber)) | ((scr == 2 & usscr == 2 & consent == 2) & is.na(subjectnumber))" ,
-  # "r11", "Número del sujeto correcto",              "substr(subjectnumber, 1, 3) != paste0(0,countrycode) & substr(subjectnumber, 1, 3) != paste0(0, 0, countrycode)",
-  "r11", "Número del sujeto correcto",              "as.numeric(substr(subjectnumber, 1, 3)) != countrycode",
-  "r12", "Grupo étnico respondido",                 "is.na(ethnicgroup)",
-  # "r13", "Número del sujeto completado",            "is.na(subjectnumber)",
-  "r13", "Código de país completado",               "is.na(countrycode)",
-  "r14", "scr completado",                          "is.na(scr)",
-  "r15", "usscr completado",                        "is.na(usscr)",
-  "r16", "consent completado",                      "is.na(consent)",
-  "r17", "ID pasiente completado",                  "is.na(patientid)",
-  "r18", "Fecha de entrevista completada",          "is.na(interview)"
+  "Regla 1", "pais fuera de la lista",                   "!(countrycode %in% c(4, 11, 14, 23, 31, 48, 54, 65, 72, 97))",
+  "Regla 2", "ID en formato DD/MM/YYYY-AA",              "stringr::str_detect(patientid,pattern = '^[:digit:]{2}/[:digit:]{2}/[:digit:]{4}-[:upper:]{2}$', negate = T) | is.na(dmy(substr(patientid,1,10),quiet = T))",
+  "Regla 3", "Fecha de entrevista en formato DD/MM/YYYY","stringr::str_detect(interview,pattern = '^[:digit:]{2}/[:digit:]{2}/[:digit:]{4}$', negate = T) | is.na(dmy(interview,quiet = T))",
+  "Regla 4", "Fechas congruentes(edad > 18)",            "as.numeric(dmy(interview,quiet = T) - dmy(substr(patientid,1,10),quiet = T))/365 < 18",
+  "Regla 5", "Grupo étnico entre las opciones",          "!(ethnicgroup %in% c(1,2,3,4))",
+  "Regla 6", "Elegible SCR dentro de opciones",          "!(scr %in% c(1,2))",
+  "Regla 7", "Elegible USSCR dentro de opciones",        "!(usscr %in% c(1,2))",
+  "Regla 8", "Consentimiento dentro de opciones",        "!(consent %in% c(1,2))",
+  "Regla 9", "Número del sujeto en el formato adecuado", "stringr::str_detect(subjectnumber,pattern = '^[:digit:]{9}$', negate = T)",
+  #Regla "r10", "Enrolamiento correcto",                   "scr != 2 | usscr != 2 | consent != 2",
+  "Regla 10", "Enrolamiento correcto",                   "((scr == 1 | usscr == 1 | consent == 1) & !is.na(subjectnumber)) | ((scr == 2 & usscr == 2 & consent == 2) & is.na(subjectnumber))" ,
+  #Regla "r11", "Número del sujeto correcto",              "substr(subjectnumber, 1, 3) != paste0(0,countrycode) & substr(subjectnumber, 1, 3) != paste0(0, 0, countrycode)",
+  "Regla 11", "Número del sujeto correcto",              "as.numeric(substr(subjectnumber, 1, 3)) != countrycode",
+  "Regla 12", "Grupo étnico respondido",                 "is.na(ethnicgroup)",
+  #Regla "r13", "Número del sujeto completado",            "is.na(subjectnumber)",
+  "Regla 13", "Código de país completado",               "is.na(countrycode)",
+  "Regla 14", "scr completado",                          "is.na(scr)",
+  "Regla 15", "usscr completado",                        "is.na(usscr)",
+  "Regla 16", "consent completado",                      "is.na(consent)",
+  "Regla 17", "ID pasiente completado",                  "is.na(patientid)",
+  "Regla 18", "Fecha de entrevista completada",          "is.na(interview)"
   )
 
 
@@ -78,12 +81,29 @@ validacion_largo <-  tidyr::pivot_longer(validacion, -registro, names_to = "regl
 
 # 2do: se grafica
 library(ggplot2)
-ggplot(validacion_largo)+
+g1 <- ggplot(validacion_largo[1:4500,])+
+  aes(x = registro, y = regla, fill = error)+
+  geom_tile(width = 0.7, height = .9, color = "black")+
+  scale_fill_manual(values = c("skyblue2","firebrick3","gray"))+
+  theme_bw()
+g2 <- ggplot(validacion_largo[4501:9000,])+
+  aes(x = registro, y = regla, fill = error)+
+  geom_tile(width = 0.7, height = .9, color = "black")+
+  scale_fill_manual(values = c("skyblue2","firebrick3","gray"))+
+  theme_bw()
+g3 <- ggplot(validacion_largo[9001:13500,])+
+  aes(x = registro, y = regla, fill = error)+
+  geom_tile(width = 0.7, height = .9, color = "black")+
+  scale_fill_manual(values = c("skyblue2","firebrick3","gray"))+
+  theme_bw()
+g4 <- ggplot(validacion_largo[13501:18000,])+
   aes(x = registro, y = regla, fill = error)+
   geom_tile(width = 0.7, height = .9, color = "black")+
   scale_fill_manual(values = c("skyblue2","firebrick3","gray"))+
   theme_bw()
 
+
+Grafico <- g1 + g2 + g3 + g4 + plot_layout(ncol = 1, guides = "collect", axes = "collect")
 
 # RESULTADOS
 
